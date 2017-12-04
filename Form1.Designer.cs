124193e612aadbd23e78bee04c085a9dd83e9845
Matt R Cole <mattcolefla@gmail.com>
2017-12-04 09:30:42 -0500

    Add files via upload
    Initial Checkin

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ActivationFunctionViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zed = new ZedGraph.ZedGraphControl();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // zed
+            // 
+            this.zed.BackColor = System.Drawing.Color.Gainsboro;
+            this.zed.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.zed.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.zed.IsAntiAlias = true;
+            this.zed.IsEnableHPan = false;
+            this.zed.IsEnableHZoom = false;
+            this.zed.IsShowPointValues = true;
+            this.zed.Location = new System.Drawing.Point(3, 3);
+            this.zed.Name = "zed";
+            this.zed.ScrollGrace = 0D;
+            this.zed.ScrollMaxX = 0D;
+            this.zed.ScrollMaxY = 0D;
+            this.zed.ScrollMaxY2 = 0D;
+            this.zed.ScrollMinX = 0D;
+            this.zed.ScrollMinY = 0D;
+            this.zed.ScrollMinY2 = 0D;
+            this.zed.Size = new System.Drawing.Size(842, 559);
+            this.zed.TabIndex = 2;
+            this.zed.UseExtendedPrintDialog = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(856, 591);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.zed);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(848, 565);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(705, 565);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 591);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Activation Function Viewer";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zed;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
